ebc3dce115c34a6e84436f9af113b6dc668478b0
oli4202 <oliurrob64@gmail.com>
2026-06-21 20:48:28 +0600

    Payment test file is added

--- src/test/java/org/example/PaymentTest.java
+++ src/test/java/org/example/PaymentTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class paymentTest {
+public class PaymentTest {
     @Test
     void testSuccessfulPayment() throws Exception {
 
